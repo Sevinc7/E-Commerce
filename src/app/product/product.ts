e506f53fb3576d569711f6f296cd221d347d5ab9
Sevinc <sevincsofie74@gmx.at>
2023-03-08 15:02:53 +0100

    1

--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -3,49 +3,49 @@ import { Iproduct } from "./iproduct";
 export const PREPARED_PRODUCTS: Iproduct[] = [
     {
         id: 0,
-        name: "pizza",
+        name: "Pizza Cardinale",
         image: "/assets/pizza.jpg",
         price: 13.49 
     },
     {
         id: 1,
-        name: "pizza1",
+        name: "Pizza with cheese and olives",
         image: "/assets/pizza1.jpg",
         price: 13.69 
     },
     {
         id: 2,
-        name: "pasta",
+        name: "Pasta with cheese and spinage",
         image: "/assets/spinat_cheese_pasta.jpg",
         price: 14.29 
     },
     {
         id: 3,
-        name: "minestrone",
+        name: "Minestrone",
         image: "/assets/minestrone.jpg",
         price: 7.29 
     },
     {
         id: 4,
-        name: "lasagne",
+        name: "Lasagne",
         image: "/assets/fleish-lasagne.jpg",
         price: 14.39 
     },  
     {
         id: 5,
-        name: "jelato",
+        name: "Gelato",
         image: "/assets/jelato.jpg",
         price: 5.79 
     },
     {
         id: 6,
-        name: "tiramisu",
+        name: "Tiramisu",
         image: "/assets/tiramisu.jpg",
         price: 6.29
     },
     {
         id: 7,
-        name: "torte_amaretti",
+        name: "Amaretti Torte",
         image: "/assets/torte_amaretti.jpg",
         price: 6.29
     }
